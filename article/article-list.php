ebaa443e45e7de41cc79f72927d19c2adfa2ac32
taiyi <taiyijob@gmail.com>
2023-07-11 13:39:02 +0800

    加入gitignore

---
+++ article/article-list.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Article List</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="container">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>文章編號</th>
+                    <th>文章標題</th>
+                    <th>文章摘要</th>
+                    <th>發佈時間</th>
+                    <th>建立時間</th>
+                    <th></th>
+                </tr>
+            </thead>
+    </div>
+</body>
+
+</html>
